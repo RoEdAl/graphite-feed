fb58bcb7833dca7b2eec0c5e91fea60c688255f3
Edmunt Pienkowsky <roed@onet.eu>
2025-03-26 11:36:22 +0100

    Improve carbon-relay-ng package
    - Use workspaces.
    - Do not create /var/spool/carbon-relay-ng directory.

--- packages/carbon-relay-ng/files/mod-tweaks.sh
+++ packages/carbon-relay-ng/files/mod-tweaks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 replace_mod() {
-    cd "$BUILD_DIR" || return 1
-    go mod edit -replace github.com/grafana/metrictank=github.com/RoEdAl/metrictank@v1.2.0-arch32 || return 1
-    go get github.com/grafana/carbon-relay-ng/cfg github.com/grafana/carbon-relay-ng/route || return 1
+    cd "$BUILD_DIR"
+    go work init .
+    go work edit -replace github.com/grafana/metrictank=github.com/RoEdAl/metrictank@v1.2.0-arch32
 }
 
 replace_mod
